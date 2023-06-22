05d1de4af2f245d48d917d11de1d2098a6ac7899
DwcManhPTIT <ducmanhcr7ngt@gmail.com>
2023-06-23 01:36:47 +0700

    2

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,4 @@
-const API_DOMAIN = "http://localhost:3009/";
+const API_DOMAIN = "https://database-json-eight.vercel.app/";
 
 export const get = async (path, options = {}) => {
   const response = await fetch(API_DOMAIN + path, options);
