672b6bfcb22f8c6fda58be74ea1bc1ee5de65e02
DwcManhPTIT <ducmanhcr7ngt@gmail.com>
2023-06-22 18:56:27 +0700

    1

---
+++ src/components/Layout/Footer/index.js
@@ -0,0 +1,15 @@
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <div className="container">
+          Copyright 2023 by dwcmanh
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer;
